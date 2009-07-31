d698b8a14203546a242c1c59ffeddd86792d9556
Shawn H Corey <shawnhcorey@gmail.com>
2009-07-31 19:48:34 -0400

    Finished the PODs

--- lib/Sub/Starter.pm
+++ lib/Sub/Starter.pm
@@ -2,7 +2,7 @@
 # --------------------------------------
 #
 #   Title: Sub Starter
-# Purpose: Creates a skeketal framework for Perl sub's.
+# Purpose: Creates a skeletal framework for Perl sub's.
 #
 #    Name: Sub::Starter
 #    File: Starter.pm
@@ -238,7 +238,7 @@ sub _parse_variable {
 #      Usage: _parse_returns( $parsed, $returns_part );
 #    Purpose: Parse the sub's return variables
 # Parameters:       $parsed -- storage hash
-#             $returns_part -- part of the usage statment before the assignment
+#             $returns_part -- part of the usage statement before the assignment
 #    Returns: none
 #
 sub _parse_returns {
@@ -300,7 +300,7 @@ sub _parse_returns {
 #      Usage: _parse_parameters( $parsed, $param_part );
 #    Purpose: Break the parameters into variables and store them.
 # Parameters:     $parsed -- storage hash
-#             $param_part -- part of the usage statment including optional parameters
+#             $param_part -- part of the usage statement including optional parameters
 #    Returns: none
 #
 sub _parse_parameters {
@@ -367,7 +367,7 @@ sub parse_usage {
     $func_part = $2;
   }
   if( $func_part =~ m{ = }msx ){
-    croak "Multiple assignments in usage statment";
+    croak "Multiple assignments in usage statement";
   }
 
   # get the name and possible object
@@ -518,7 +518,7 @@ sub _fill_out_arenot {
 #
 sub _fill_out_each {
   my $max_len = shift @_;
-  my $format = shift @_;
+  my $format = shift @_ || '%s';
   my @list   = @_;
   my $text   = undef;
 
@@ -527,14 +527,10 @@ sub _fill_out_each {
   $format =~ s{ \\ ($String_escapes) }{$String_escapes{$1}||$1}egmsx;
 
   # print 'each: ', Dumper $format, \@list;
-  if( $format ){
-    if( $format =~ m{ \* }msx ){
-      $text = [ map { sprintf( $format, $max_len, $_ ) } @list ];
-    }else{
-      $text = [ map { sprintf( $format, $_ ) } @list ];
-    }
+  if( $format =~ m{ \* }msx ){
+    $text = [ map { sprintf( $format, $max_len, $_ ) } @list ];
   }else{
-    $text = [ @list ];
+    $text = [ map { sprintf( $format, $_ ) } @list ];
   }
 
   return $text;
@@ -586,7 +582,7 @@ sub _fill_out_rest {
 #
 sub _fill_out_list {
   my $max_len   = shift @_;
-  my $separator = shift @_ || '';
+  my $separator = shift @_ || ' ';
   my @list      = @_;
 
   return [ join( $separator, @list ) ];
@@ -690,16 +686,14 @@ sub _fill_out_returns {
 #
 sub _fill_out_definitions {
   my $self      = shift @_;
-  my $format    = shift @_;
+  my $format    = shift @_ || '%s = %s';
   my @list      = ();
   my $text      = [];
   my %seen = ();
 
   # print 'self: ', Dumper $self;
 
-  if( $format ){
-    $format =~ s{ \\ ($String_escapes) }{$String_escapes{$1}||$1}egmsx;
-  }
+  $format =~ s{ \\ ($String_escapes) }{$String_escapes{$1}||$1}egmsx;
 
   # do parameters
   if( $self->{-object} ){
@@ -720,14 +714,10 @@ sub _fill_out_definitions {
     if( $item->{-type} eq 'array' || $item->{-type} eq 'hash' ){
       $value = '@_';
     }
-    if( $format ){
-      if( $format =~ m{ \* }msx ){
-        push @$text, sprintf( $format, $self->{-max_variable}, $item->{-variable}, $value );
-      }else{
-        push @$text, sprintf( $format, $item->{-variable}, $value );
-      }
+    if( $format =~ m{ \* }msx ){
+      push @$text, sprintf( $format, $self->{-max_variable}, $item->{-variable}, $value );
     }else{
-      push @$text, "$tiem->{-variable} = $value";
+      push @$text, sprintf( $format, $item->{-variable}, $value );
     }
   }
 
@@ -753,14 +743,10 @@ sub _fill_out_definitions {
     }else{
       $value = 'undef';
     }
-    if( $format ){
-      if( $format =~ m{ \* }msx ){
-        push @$text, sprintf( $format, $self->{-max_variable}, $item->{-variable}, $value );
-      }else{
-        push @$text, sprintf( $format, $item->{-variable}, $value );
-      }
+    if( $format =~ m{ \* }msx ){
+      push @$text, sprintf( $format, $self->{-max_variable}, $item->{-variable}, $value );
     }else{
-      push @$text, "$tiem->{-variable} = $value";
+      push @$text, sprintf( $format, $item->{-variable}, $value );
     }
   }
 
@@ -814,7 +800,7 @@ __END__
 
 =head1 NAME
 
-Sub::Starter - Creates a skeketal framework for Perl sub's.
+Sub::Starter - Creates a skeletal framework for Perl sub's.
 
 =head1 VERSION
 
@@ -839,7 +825,7 @@ uses the make-a-reference-to notation for references.  This
 is to give a clear indication of what the references is to.
 
 The following variables can be used for the parameters,
-including the opptional ones, and the returns;
+including the optional ones, and the returns;
 
 =over 4
 
@@ -872,15 +858,21 @@ The C<assignment> is one of: C<=>, C<.=>, or C<+=>
 The C<returns_list> and the C<parameters_list> must be a
 list of scalars and references except for the last element,
 which can be an array or hash.  If the
-C<optional_pararmeters> list is present, the
+C<optional_parameters> list is present, the
 C<parameters_list> cannot end with an array or hash.
 Instead, the C<optional_parameters> list can.  These lists
 are variables separated by a comma.
 
+=head3 Examples of usage statements
+
+  $text | @text = trim( @text );
+
+  \%options = $object->get_options( ; @option_names );
+
 =head2 Templates
 
 Templates are used by the C<fill_out()> method to determine
-how the output will look.  Tehy are array of lines.  Each
+how the output will look.  They are array of lines.  Each
 line may contain one, and only one, directive.  Lines
 without directives are copied verbatim to the output.  Some
 directives has arguments.  Directive are distinguished by
@@ -896,24 +888,103 @@ It has no arguments.
 
 =item The usage directive
 
-The C<usage> directive is replaced with the usage statment.
+The C<usage> directive is replaced with the usage statement.
 It has no arguments.
 
 =item The parameters directive
 
-TBD
+This is used to list the parameters.
+It has one mandatory argument and one optional one.
+The mandatory argument tells how and how much to display.
+It can be C<are>, C<arenot>, C<each>, C<first>, C<rest>, or C<list>.
+
+If it is C<are> then the line is outputted if there are some
+parameters.  It can have an optional format argument (see
+below).
+
+If it is C<arenot> then the line is outputted if there are
+not any parameters.  It can have an optional format argument
+(see below).
+
+The C<list> argument replaces the directive with a list of
+the parameters.  The optional argument is the list
+separator.  The default separator is a single space.
+
+The C<each> outputs the line for each item in the list.
+The C<first> outputs the line for the first item in the list.
+The C<rest> outputs the line for all but the first item in the list.
+If their list is empty, the line is not outputted.
+They have an optional argument that formats the variable in the output.
+
+The format argument translate '\s', '\t', and '\n' into
+space, tab, and newline characters respectively.  If it's
+part of a listing, it can have '%s', '%*s', or '%-*s' to
+determine how the item will be displayed.  The '%s' will
+simply display it.  This is the default.  The '%*s' will
+display it, right-justified in a column wide enough for the
+widest item.  The '%-*s' will display it, left-justified in
+a column wide enough for the widest item.
 
 =item The returns directive
 
-TBD
+This is used to list the returns.  It takes the same
+arguments as the C<parameters> directive.
+
+In addition, it has the C<expression> argument that creates
+an expression for a return statement.
 
 =item The definitions directive
 
-TBD
+The definitions directive will output all the parameters and
+returns, one per line with a Perl assignment.  It has an
+optional format which may be '%s\s=\s%s', '%*s\s=%s', or
+'%-*s\s=\s%s'.  The first is the default.  The second
+right-justifies the variable.  The last left-justifies it.
 
 =back
 
-TBD
+=head3 Example of template: sub
+
+  # --------------------------------------
+  #       Name: \e[1m(name)\e[0m
+  #      Usage: \e[1m(usage)\e[0m
+  #    Purpose: TBD
+  # Parameters: (none)\e[1m(parameters arenot)\e[0m
+  # Parameters: \e[1m(parameters first %*s)\e[0m -- TBD
+  #             \e[1m(parameters rest %*s)\e[0m -- TBD
+  #    Returns: (none)\e[1m(returns arenot)\e[0m
+  #    Returns: \e[1m(returns first %*s)\e[0m -- TBD
+  #             \e[1m(returns rest %*s)\e[0m -- TBD
+  #
+  sub \e[1m(name)\e[0m {
+    my \e[1m(definitions %-*s\s=\s%s)\e[0m;
+
+    return\e[1m(returns expression)\e[0m;
+  }
+
+=head3 Example of template: pod
+
+  =head2 \e[1m(name)\e[0m()
+
+  TBD
+
+  =head3 Usage
+
+    \e[1m(usage)\e[0m
+
+  =head3 Parameters
+
+  (none)\e[1m(parameters arenot)\e[0m
+  =over 4\e[1m(parameters are \n)\e[0m
+  =item \e[1m(parameters each %s\n\nTBD\n)\e[0m
+  =back\e[1m(parameters are)\e[0m
+
+  =head3 Returns
+
+  (none)\e[1m(returns arenot)\e[0m
+  =over 4\e[1m(returns are \n)\e[0m
+  =item \e[1m(returns each %s\n\nTBD\n)\e[0m
+  =back\e[1m(returns are)\e[0m
 
 =head1 METHODS
 
@@ -938,7 +1009,7 @@ space.  Those that start with a minus sign are part of its
 API.  Those that start with a space are for internal storage
 and should not be changed.
 
-If the application needs to store infomation in the object,
+If the application needs to store information in the object,
 start the key with an alphanumeric character.  If a derived
 class needs an attribute, start it with two minus signs.
 
@@ -972,8 +1043,8 @@ C<Sub::Scalar>.
 
 =item -parameters
 
-This is a list of hashes decribing the parameters.  Each has
-a:
+This is a list of hashes describing the parameters.  Each
+has a:
 
 C<-type>: C<scalar>, C<array>, C<hash>, C<scalar_ref>,
 C<array_ref>, C<hash_ref>, C<code_ref>, or C<typeglob>
@@ -993,7 +1064,7 @@ then this attribute is the null string.
 
 =item -returns
 
-This is a list of hashes decribing the returns.  They are
+This is a list of hashes describing the returns.  They are
 described the same way as the parameters except they cannot
 be optional.
 
@@ -1007,7 +1078,7 @@ be optional.
 
 =item $starter_sub
 
-This is the new object blessed to the C<Sub::Staarter> class.
+This is the new object blessed to the C<Sub::Starter> class.
 
 =back
 
